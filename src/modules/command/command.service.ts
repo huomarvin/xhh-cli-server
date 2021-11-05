ee846a8dada3586d7fed4879261a71d2cfbbc579
xhh <huo.marvin@gmail.com>
2021-11-05 17:22:49 +0800

    feat: command dynamic

--- src/modules/command/command.service.ts
+++ src/modules/command/command.service.ts
@@ -1,39 +1,15 @@
+import { CommandDocument, Command } from '@/schemas/command.schema';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class CommandService {
-  findAll() {
-    return {
-      results: [
-        {
-          command: 'init',
-          commandContent: 'init',
-          options: [
-            {
-              flags: '-f, --force',
-              description: '是否强制初始化',
-              //   defaultValue?: string | boolean;
-            },
-          ],
-          description: '初始化一个项目',
-          packageName: '@xhh-cli-dev/init',
-          packageVersion: 'latest',
-        },
-        {
-          command: 'kill',
-          commandContent: 'kill <port>',
-          //   options: [
-          //     {
-          //       flags: '-p, --port <port>',
-          //       description: '杀掉进程的端口号',
-          //       //   defaultValue?: string | boolean;
-          //     },
-          //   ],
-          description: '删掉对应端口号的进程',
-          packageName: '@xhh-cli-dev/kill',
-          packageVersion: 'latest',
-        },
-      ],
-    };
+  constructor(
+    @InjectModel(Command.name) private model: Model<CommandDocument>,
+  ) {}
+
+  async findAll(): Promise<Command[]> {
+    return this.model.find().exec();
   }
 }
